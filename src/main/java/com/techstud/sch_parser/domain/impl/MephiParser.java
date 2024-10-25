46c7dc073d4a7ce70efbbf2cd79e34e21b958963
EldarKabarxx <eldar200715@gmail.com>
2024-10-26 02:02:40 +0300

    SCHU-29 Automatic week detection

--- src/main/java/com/techstud/sch_parser/domain/impl/MephiParser.java
+++ src/main/java/com/techstud/sch_parser/domain/impl/MephiParser.java
@@ -9,7 +9,11 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Component;
 
+import java.text.MessageFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.TimeZone;
 
 @Component
 @Slf4j
@@ -20,12 +24,47 @@ public class MephiParser implements Parser {
 
     @Override
     public Schedule parseSchedule(String groupId) throws Exception {
-        String evenUrl = "https://home.mephi.ru/study_groups/" + groupId + "/schedule?period=0";
-        String oddUrl = "https://home.mephi.ru/study_groups/" + groupId + "/schedule?period=1";
+        String[] parseWeeks = getCurrentWeekNumbers(TimeZone.getTimeZone("Europe/Moscow"));
+        final String[] evenParameters = {String.valueOf(groupId), parseWeeks[0]};
+        final String[] oddParameters = {String.valueOf(groupId), parseWeeks[1]};
+
+        String mephiScheduleUrl = "https://home.mephi.ru/study_groups/{0}/schedule?period={1}";
+
+        final String evenUrl = MessageFormat.format(mephiScheduleUrl, evenParameters[0], evenParameters[1]);
+        final String oddUrl = MessageFormat.format(mephiScheduleUrl, oddParameters[0], oddParameters[1]);
+
+        log.info("Even week URL: " + evenUrl);
+        log.info("Odd week URL: " + oddUrl);
 
         Document evenDoc = Jsoup.connect(evenUrl).get();
         Document oddDoc = Jsoup.connect(oddUrl).get();
 
         return mappingService.mapMephiToSchedule(List.of(evenDoc, oddDoc));
+    }
+
+    private String[] getCurrentWeekNumbers(TimeZone timeZone) {
+        log.info(timeZone.toString());
+        String[] parseWeeks;
+        Calendar currentCalendar = Calendar.getInstance();
+        currentCalendar.setTimeZone(timeZone);
+        Calendar startEtudeCalendar;
+
+        if (currentCalendar.get(Calendar.MONTH) >= Calendar.SEPTEMBER) {
+            startEtudeCalendar = new GregorianCalendar(currentCalendar.get(Calendar.YEAR), Calendar.SEPTEMBER, 1);
+        } else {
+            startEtudeCalendar = new GregorianCalendar(currentCalendar.get(Calendar.YEAR), Calendar.FEBRUARY, 1);
+        }
+        startEtudeCalendar.setTimeZone(timeZone);
+
+        int currentGlobalNumberWeek = currentCalendar.get(Calendar.WEEK_OF_YEAR);
+        int startGlobalNumberWeek = startEtudeCalendar.get(Calendar.WEEK_OF_YEAR);
+        int currentEtudeWeek = currentGlobalNumberWeek - startGlobalNumberWeek;
+
+        if (currentEtudeWeek % 2 != 0) {
+            parseWeeks = new String[]{String.valueOf(currentEtudeWeek), String.valueOf(currentEtudeWeek + 1)};
+        } else {
+            parseWeeks = new String[]{String.valueOf(currentEtudeWeek - 1), String.valueOf(currentEtudeWeek)};
+        }
+        return parseWeeks;
     }
 }
